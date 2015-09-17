375ae99ccead348080cb852e5065814dc6c258f5
Todd Halkowski <thalkowski@virtusa.com>
2015-09-17 14:45:00 -0500

    To Do List app with permanent storage and swipe to delete

---
+++ SecondViewController.swift
@@ -0,0 +1,59 @@
+//
+//  SecondViewController.swift
+//  To Do List
+//
+//  Created by Todd Halkowski
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    
+    @IBOutlet var item: UITextField!
+    
+    
+    @IBAction func addItem(sender: AnyObject) {
+        
+        toDoList.append(item.text)
+        
+        item.text = ""
+        
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.item.delegate = self
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent)
+    {
+  
+        
+       self.view.endEditing(true)
+        
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        
+        item.resignFirstResponder()
+        return true
+        
+    }
+    
+
+}
+
